d3fe88e1bb540af6fb4019662d924ff4e3686d9a
耿飞阳 <1053221101@qq.com>
2019-04-28 10:26:31 +0800

    '获取全部分类'

--- controller/repairType.js
+++ controller/repairType.js
@@ -7,4 +7,20 @@ const router  = express.Router()
 
 const repairTypeModel = require('../model/repairType')
 
+/**
+ * 获取所有报修类型
+ */
+router.get('/getAll', async (req, res, next) => {
+    try {
+        let repairTypes = await repairTypeModel.find()
+        res.json({
+            code: 200,
+            data: repairTypes,
+            msg: '报修分类'
+        })
+    } catch (e) {
+        next(e)
+    }
+})
+
 module.exports = router
