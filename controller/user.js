1298247ceb22226032512f792bde056e5dac3c0c
Tiff-wyr <18438610510@qq.com>
2019-04-29 12:49:37 +0800

    管理员接口

--- controller/user.js
+++ controller/user.js
@@ -63,5 +63,153 @@ router.post('/adminLogin', async (req, res, next) => {
         next(e)
     }
 })
+/**
+ * 添加维修人员
+ * @type {Router|router|*}
+ */
+router.post('/addRepair' , async (req,res,next) =>{
+    try{
+        let {name,phone,address,sex} =req.body
+       let userInfo = await userModel.findOne({phone})
+        if(userInfo){
+            res.json({
+                code :301,
+                msg:"已存在"
+            })
+        }else{
+            await userModel.create({name,phone,address,sex,level:1})
+            res.json({
+                code :200,
+                msg:"添加成功"
+            })
+        }
+    }catch(e){
+        next(e)
+    }
+})
+/**
+ * 修改维修人员
+ * @type {Router|router|*}
+ */
+router.post('/updateRepair', async (req,res,next) =>{
+    try {
+        let {_id,name,phone,address,sex} = req.body
+        await userModel.updateOne({_id:_id},{$set: {name,phone,address,sex}})
+        res.json({
+            code:200,
+            msg:"修改成功"
+        })
+    }catch(e){
+        next(e)
+    }
+})
+/**
+ * 删除维修人员
+ * @type {Router|router|*}
+ */
+router.post('/delRepair',async (req,res,next) =>{
+    try {
+        let {_id}=req.body
+        await userModel.deleteOne({_id:_id})
+        res.json({
+            code:200,
+            msg:"删除成功"
+        })
+    }catch(e){
+        next(e)
+    }
 
+
+})
+/**
+ * 获取维修人员列表
+ * @type {Router|router|*}
+ */
+router.get('/getRepair', async (req,res,next) =>{
+    try {
+        let repair = await userModel.find({level:1})
+        res.json({
+            code: 200,
+            data: repair,
+            msg: '获取维修人员成功'
+        })
+    }catch(e){
+        next(e)
+    }
+})
+
+/**
+ * 添加管理员
+ * @type {Router|router|*}
+ */
+router.post('/addAdmin' , async (req,res,next) =>{
+    try{
+        let {name,phone,address,sex} =req.body
+        let userInfo = await userModel.findOne({phone})
+        if(userInfo){
+            res.json({
+                code :301,
+                msg:"已存在"
+            })
+        }else{
+            await userModel.create({name,phone,address,sex,level:0})
+            res.json({
+                code :200,
+                msg:"添加成功"
+            })
+        }
+    }catch(e){
+        next(e)
+    }
+})
+/**
+ * 修改管理员
+ * @type {Router|router|*}
+ */
+router.post('/updateAdmin', async (req,res,next) =>{
+    try {
+        let {_id,name,phone,address,sex} = req.body
+        await userModel.updateOne({_id:_id},{$set: {name,phone,address,sex}})
+        res.json({
+            code:200,
+            msg:"修改成功"
+        })
+    }catch(e){
+        next(e)
+    }
+})
+/**
+ * 删除管理员
+ * @type {Router|router|*}
+ */
+router.post('/delAdmin',async (req,res,next) =>{
+    try {
+        let {_id}=req.body
+        await userModel.deleteOne({_id:_id})
+        res.json({
+            code:200,
+            msg:"删除成功"
+        })
+    }catch(e){
+        next(e)
+    }
+
+
+})
+/**
+ * 获取管理员
+ * @type {Router|router|*}
+ */
+router.get('/getAdmin', async (req,res,next) =>{
+    try {
+        let repair = await userModel.find({level:0})
+        res.json({
+            code: 200,
+            data: repair,
+            msg: '获取管理员成功'
+        })
+    }catch(e){
+        next(e)
+    }
+})
 module.exports = router
