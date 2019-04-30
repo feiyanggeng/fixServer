232289a80c13a55c970190557571ed584a17abfd
耿飞阳 <1053221101@qq.com>
2019-04-30 21:54:05 +0800

    '修改model'

--- model/maintain.js
+++ model/maintain.js
@@ -16,13 +16,31 @@ var maintain = new Schema({
         type: Schema.ObjectId,
         ref: 'user'
     },              // 维修员Id
-    images: String, //维修情况图片详情（‘http：//XXXX,http://XXXXX’）多张图片以逗号隔开
-    remark: String, // 维修单备注
-    address: String, // 维修地址详情
-    passTime: String, // 维修单通过时间
+    images:  {
+        type: String,
+        default: ''
+    }, //维修情况图片详情（‘http：//XXXX,http://XXXXX’）多张图片以逗号隔开
+    remark:  {
+        type: String,
+        default: ''
+    }, // 维修单备注
+    address:  {
+        type: String,
+        default: ''
+    }, // 维修地址详情
+    passTime: {
+        type: String,
+        default: ''
+    }, // 维修单通过时间
     status: Number,      // 维修单状态（1：已提交， 2： 审核通过，0： 审核不通过）
-    comment: String,     // 评价
-    level: Number           //评价的星级（0,1,2,3,4,5）
+    comment:  {
+        type: String,
+        default: ''
+    },     // 评价
+    level:  {
+        type: Number,
+        default: ''
+    }           //评价的星级（0,1,2,3,4,5）
 }, { timestamps: { createdAt: 'createdTime'}})
 
 module.exports = mongoose.model('maintain', maintain)
--- model/repair.js
+++ model/repair.js
@@ -17,11 +17,26 @@ var repair = new Schema({
         type: Schema.ObjectId,
         ref: 'repairType'
     },              // 报修类型
-    images: String, //报修情况图片详情（‘http：//XXXX,http://XXXXX’）多张图片以逗号隔开
-    remark: String, // 报修单备注
-    address: String, // 报修地址详情
-    passTime: String, // 维修单通过时间
-    rejectMeg:String, //驳回理由
+    images: {
+        type: String,
+        default: ''
+    }, //报修情况图片详情（‘http：//XXXX,http://XXXXX’）多张图片以逗号隔开
+    remark:  {
+        type: String,
+        default: ''
+    }, // 报修单备注
+    address:  {
+        type: String,
+        default: ''
+    }, // 报修地址详情
+    passTime:  {
+        type: String,
+        default: ''
+    }, // 维修单通过时间
+    rejectMeg: {
+        type: String,
+        default: ''
+    }, //驳回理由
     status: Number      // 报修单状态（0： 审核不通过，1：已提交， 2： 审核通过（未接单），3： 已接单， 4： 维修完毕（未评价）， 5： 已评价
 }, { timestamps: { createdAt: 'createdTime'}})
 
