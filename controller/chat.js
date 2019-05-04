1fc0b8d69abb182d90c4916ad9ac936dcf9c919b
Tiff-wyr <18438610510@qq.com>
2019-05-04 20:17:00 +0800

    '报表'

--- controller/chat.js
+++ controller/chat.js
@@ -12,46 +12,47 @@ const repairTypeModel = require('../model/repairType')
  */
 router.get('/getType',async(req,res,next) =>{
     try{
-        let {month} =req.query
+        let {month = 0} =req.query
         let time = new Date(month)
         let Year =time.getFullYear()
         let Month = time.getMonth()+1
         let Next = Month+1
         let timeCon =Year + '-' + Month + '-' + '1' + ' 00:00:00'
         let nextTime =Year + '-' + Next + '-' + '1' + ' 00:00:00'
-
-        let typesCount = await repairModel.aggregate([{$group: {
-                    _id: "$type",
-                    count: {$sum: 1}
-                }},
-        {$match: {createdTime: { $gte : timeCon, $lt : nextTime }}}])
-            let types = await repairTypeModel.find()
-            let seriesData = []
-            types.forEach(item => {
-                let _index = -1
-                typesCount.forEach((count, index) => {
-                    if (item._id.toString() === count._id.toString()) {
-                        _index = index
-                    }
-                })
-                seriesData.push({
-                    name: item.name,
-                    value: _index === -1 ? 0 : typesCount[_index].count
-                })
-            })
-            let legendData = types.map(item => {
-                return item.name
-            })
-            res.json({
-                code: 200,
-                msg: '报表数据',
-                data: {
-                    typesCount,
-                    types,
-                    seriesData,
-                    legendData
+        let typesCount = []
+        if (month = 0) {
+             typesCount = await repairModel.aggregate([
+                {$group: {_id: "$type",count: {$sum: 1}}}])
+        } else {
+            typesCount = await repairModel.aggregate([
+                {$group: {_id: "$type",count: {$sum: 1}}},
+                {$match: {createdTime: { $gte : timeCon, $lt : nextTime }}}])
+        }
+        let types = await repairTypeModel.find()
+        let seriesData = []
+        types.forEach(item => {
+            let _index = -1
+            typesCount.forEach((count, index) => {
+                if (item._id.toString() === count._id.toString()) {
+                    _index = index
                 }
             })
+            seriesData.push({
+                name: item.name,
+                value: _index === -1 ? 0 : typesCount[_index].count
+            })
+        })
+        let legendData = types.map(item => {
+            return item.name
+        })
+        res.json({
+            code: 200,
+            msg: '报表数据',
+            data: {
+                seriesData,
+                legendData
+            }
+        })
 
     }catch(e){
         next(e)
