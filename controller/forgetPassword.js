1be9dea00d0e341453dab3d3930f58686c2394ef
Tiff-wyr <18438610510@qq.com>
2019-05-06 15:58:39 +0800

    '测试发送数据'

---
+++ controller/forgetPassword.js
@@ -0,0 +1,44 @@
+/**
+ * Created by GFY on 2019-04-19.
+ * 调用钉钉接口
+ */
+const express = require('express')
+const router = express.Router()
+const https = require('https')
+
+const sid = "ab6ec8db929dd73c97948eb9b1405fa7"
+const  token = "d71da22ef74b69708bda0ce4ace0e017"
+const  appid = "0c1ef18e555f48148dd1cac1f9c32ebf"
+const  templateid ="462575"
+
+router.get('/forget', (req, res) => {
+    let {mobile} = req.query
+    let postData=JSON.stringify({
+        sid,
+        token,
+        appid,
+        templateid,
+        mobile,
+    })
+    const options = {
+        hostname: 'open.ucpaas.com',
+        port: 3030,
+        path: '/ol/sms/sendsms',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': Buffer.byteLength(postData)
+        }
+    };
+
+    const request = https.request(options, (data) => {
+        data.on('data', (chunk) => {
+            let response =JSON.parse(chunk.toString())
+        });
+    });
+
+    request.end();
+
+})
+
+module.exports = router
