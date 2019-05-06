b7943e47e9cd430d8f6cedb572f5a5130a1e72d1
Tiff-wyr <18438610510@qq.com>
2019-05-06 16:21:09 +0800

    '测试发送数据'

--- controller/forgetPassword.js
+++ controller/forgetPassword.js
@@ -4,44 +4,36 @@
  */
 const express = require('express')
 const router = express.Router()
-const https = require('https')
+const axios = require('axios')
 
 const sid = "ab6ec8db929dd73c97948eb9b1405fa7"
-const  token = "d71da22ef74b69708bda0ce4ace0e017"
-const  appid = "0c1ef18e555f48148dd1cac1f9c32ebf"
-const  templateid ="462575"
+const token = "d71da22ef74b69708bda0ce4ace0e017"
+const appid = "0c1ef18e555f48148dd1cac1f9c32ebf"
+const templateid ="462575"
 
 router.get('/forget', (req, res) => {
     let {mobile} = req.query
-    let postData=JSON.stringify({
+    let postData={
         sid,
         token,
         appid,
         templateid,
-        mobile,
-    })
-    const options = {
-        hostname: 'open.ucpaas.com',
-        port: 3030,
-        path: '/ol/sms/sendsms',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Content-Length': Buffer.byteLength(postData)
-        }
-    };
-
-    const request = https.request(options, (data) => {
-        data.on('data', (chunk) => {
-            let response =JSON.parse(chunk.toString())
+        mobile
+    }
+    console.log(mobile)
+    axios.post('https://open.ucpaas.com/ol/sms/sendsms', postData).then(response => {
+        if (response.data.code === '000000') {
             res.json({
-                code: 200,
-                data: response
-            })
-        });
-    });
-
-    request.end();
+                message: '发送成功',
+                code: response.data
+            });
+        } else {
+            res.json({
+                message: '请求失败',
+                code: response.data
+            });
+        }
+    })
 
 })
 
