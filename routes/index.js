24230413d461e2b9ada7289417baf4d5437a25bb
耿飞阳 <1053221101@qq.com>
2019-05-22 13:57:19 +0800

    '整合'

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const path = require('path')
 
 const token = require('../utils/getToken')
 const repair = require('../controller/repair')
@@ -21,7 +22,10 @@ router.get('/getToken', (req, res) => {
         msg: '获取成功'
     })
 })
-
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.sendFile(path.resolve(__dirname, './admin.html'))
+});
 router.use('/dd',ddAuth)        //调用dd方法接口
 router.use('/message',message)        //获取短信验证码接口
 router.use('/repair', checkSession, repair)    // 调用报修单接口
